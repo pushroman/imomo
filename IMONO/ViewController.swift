770759842b4d7042db27e4f46fff341099e24a89
Roman Pushkarev <88341608+pushroman@users.noreply.github.com>
2025-04-05 23:17:30 +0300

    Initial Commit

---
+++ IMONO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMONO
+//
+//  Created by Роман Пушкарев on 05.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
